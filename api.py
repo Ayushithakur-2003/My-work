69e5ce3ca5a7a70e945c52ec318806fa3600947b
Ayushithakur-2003 <96823295+Ayushithakur-2003@users.noreply.github.com>
2022-07-29 06:31:19 +0530

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-import requests
-response = requests.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-print(response)
-if response:
-  print('Request is successful.')
-else:
-  print('Request returned an error.')
