da2ef45ff37944cf1d921f94f900eae0f30f4217
Ayushithakur-2003 <96823295+Ayushithakur-2003@users.noreply.github.com>
2022-08-26 17:15:35 +0530

    Delete Store directory

--- Store/src/main/java/com/store/Store/repository/MySqlrepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.Store.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.Store.model.userpersonaldetails;
-
-public interface MySqlrepository extends JpaRepository <userpersonaldetails,Integer>{
-    
-}
